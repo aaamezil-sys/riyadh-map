91f492f520b9de7305a6569436222d26d161101c
Ahmed Mezil <ahmedmezil@ahmeds-mbp.home>
2026-02-08 11:49:58 -0500

    Hard reset map page to short version

--- app/15-min-delivery/page.js
+++ app/15-min-delivery/page.js
@@ -1,16 +1,8 @@
 'use client';
-
 import dynamic from 'next/dynamic';
 
-const RiyadhIsochrone = dynamic(() => import('../../components/RiyadhIsochrone'), { 
-  ssr: false,
-  loading: () => <div style={{color: 'white', padding: '20px'}}>Loading Logistics Engine...</div>
-});
+const RiyadhIsochrone = dynamic(() => import('../../components/RiyadhIsochrone'), { ssr: false });
 
-export default function MapPage() {
-  return (
-    <div style={{ width: '100vw', height: '100vh', background: 'black' }}>
-      <RiyadhIsochrone />
-    </div>
-  );
+export default function Page() {
+  return <RiyadhIsochrone />;
 }
